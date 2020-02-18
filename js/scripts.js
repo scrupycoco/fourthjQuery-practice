55f18f1887e9155bf37838f46dc33dffd5f14abc
Pip Bbaale <namugera.philip@gmail.com>
2020-02-18 15:47:34 +0300

    final

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
 $(document).ready(function(){
     $("button#hello").click(function(){
-        $("ul").prepend("<li>Hello</>");
+        $("ul#user").prepend("<li>Hello</>");
+        $("ul#webpage").prepend("<li>Why hello there!</>");
+        $("ul#user").children("li").first().click(function() {
+            $(this).remove();
+          });
+          $("ul#webpage").children("li").first().click(function() {
+            $(this).remove();
+          });
     });
     $("button#goodbye").click(function(){
-        $("ul").prepend("<li>Goodbye</li>");
+        $("ul#user").prepend("<li>Goodbye</li>");
+        $("ul#webpage").prepend("<li>Goodbye, dear user!</>");
     });
     $("button#stop").click(function(){
-        $("ul").prepend("<li>Stop copying me</li>");
+        $("ul#user").prepend("<li>Stop copying me</li>");
+        $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</>");
     });
 
 });
